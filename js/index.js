d74d1d461c405b068b19cdc2fbb30a9663828b2e
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-08-18 20:42:08 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -14,3 +14,12 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
