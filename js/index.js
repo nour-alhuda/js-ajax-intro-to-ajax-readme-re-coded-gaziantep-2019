d56e153481e0a6d7cbeee657b41d5c50f974bb8a
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-08-18 20:46:07 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,27 +1,10 @@
 // your code here
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
-}
-function showRepositories() {
-  console.log(this.responseText);
-}
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
-}
-function showRepositories() {
-  console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
-  document.getElementById('repositories').innerHTML = repoList;
 }
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
